423e11f8643316d552086b47212a732df90e4585
eldianteenswagger <craken156@gmail.com>
2025-05-21 15:21:05 +0200

    ah

--- build/overlap.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-X, Y, Z = np.loadtxt('overlap.txt', delimiter=' ', unpack=True)
-
-ax = plt.axes(projection='3d')
-graph = plt.plot(X, Y, Z, linestyle=" ", marker="o")
-plt.show()
-#plt.savefig('figure1.pdf')
